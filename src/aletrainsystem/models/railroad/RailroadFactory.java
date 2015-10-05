8a05193f97dd4459a0f0f9668c668e96d4134b31
Henrik Heggelund Svendsen <hsvendsen@gmail.com>
2015-10-05 11:47:59 +0200

    Referenced BlueBrick4J, add map

--- src/aletrainsystem/models/railroad/RailroadFactory.java
+++ src/aletrainsystem/models/railroad/RailroadFactory.java
@@ -1,17 +1,23 @@
 package aletrainsystem.models.railroad;
 
-import java.io.File;
-
 import aletrainsystem.enums.PointSwitchConnectorEnum;
 import aletrainsystem.models.PointSwitchId;
 import aletrainsystem.models.railroad.PointSwitch.PointSwitchConnector;
+import bluebrick4j.conversion.BbmParser;
 
 public class RailroadFactory {
 	
 	private Railroad railroad;
 
-	public RailroadFactory(File bbmFile){
+	public RailroadFactory(){
 		railroad = new Railroad();
+	}
+	
+	public Railroad convertBbmFile(String bbmFilePath) {
+		Railroad railroad = new Railroad();
+		BbmParser.loadMapFromFile("resources/maps/map.bbm");
+		
+		return railroad;
 	}
 	
 	private void addRailLeg(PointSwitch endpoint1, PointSwitchConnectorEnum endpoint1Type, PointSwitch endpoint2, PointSwitchConnectorEnum endpoint2Type, int length){
