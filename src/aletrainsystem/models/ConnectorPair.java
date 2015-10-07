53af0295188b4d0c90926075c52fc6420b43cca2
Henrik Heggelund Svendsen <hsvendsen@gmail.com>
2015-10-07 11:38:16 +0200

    Remove redundant method

--- src/aletrainsystem/models/ConnectorPair.java
+++ src/aletrainsystem/models/ConnectorPair.java
@@ -8,15 +8,7 @@ public class ConnectorPair extends Pair<PointSwitchConnector, PointSwitchConnect
 	public ConnectorPair(PointSwitchConnector first, PointSwitchConnector second) {
 		super(first, second);
 	}
-	
-	public PointSwitchConnectorEnum getTypeIfIdentical() {
-		if (first.getConnectorType() == second.getConnectorType()) {
-			return first.getConnectorType();
-		}
 		
-		return null;
-	}
-	
 	public boolean bothOfType(PointSwitchConnectorEnum connectorType) {
 		return first.getConnectorType() == connectorType 
 				&& second.getConnectorType() == connectorType;
